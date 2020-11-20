46892bfd6979052281ef57cc6767311721d3989c
Braxton Shoop <braxton.shoop@gmail.com>
2020-11-20 17:32:22 -0600

    phase 5 part 2 completed

--- Program.cs
+++ Program.cs
@@ -14,28 +14,31 @@ namespace PlanYourHeist
             ");
             
             List<Accomplice> accomplices = new List<Accomplice>() {};
+            
+                while(true)
+                {
+                    Console.Write("Please enter your team member's name . . . ");
+                    string NameResponse = Console.ReadLine();
+                    if(NameResponse == "")break;
 
-            while(true)
-            {
-                Console.Write("Please enter your team member's name . . . ");
-                string NameResponse = Console.ReadLine();
-                if(NameResponse == "")break;
-
-                Console.Write("Please enter an integer representing your team member's skill level . . . ");
-                int SkillLevelResponse = int.Parse(Console.ReadLine());
+                    Console.Write("Please enter an integer representing your team member's skill level . . . ");
+                    int SkillLevelResponse = int.Parse(Console.ReadLine());
+                    
+                    Console.Write("Please enter a number, including decimal points if appropriate, representing your team member's courage factor . . . ");
+                    double CourageFactorResponse = double.Parse(Console.ReadLine());
                 
-                Console.Write("Please enter a number, including decimal points if appropriate, representing your team member's courage factor . . . ");
-                double CourageFactorResponse = double.Parse(Console.ReadLine());
-            
-                accomplices.Add(
-                    new Accomplice() {
-                        Name = NameResponse,
-                        SkillLevel = SkillLevelResponse,
-                        CourageFactor = CourageFactorResponse
-                    }
-                );
-            }
-            
+                    accomplices.Add(
+                        new Accomplice() {
+                            Name = NameResponse,
+                            SkillLevel = SkillLevelResponse,
+                            CourageFactor = CourageFactorResponse
+                        }
+                    );
+                }
+            Console.Write("How many times would you like to run this trial?");
+            int scenarios = int.Parse(Console.ReadLine());
+           
+
             int skill = accomplices.Sum(member => member.SkillLevel);
             //Console.WriteLine($"Team Skill Level:  {skill}");
 
