54442ed05c68cdfedf36629d1536fc5fbd47f476
Braxton Shoop <braxton.shoop@gmail.com>
2020-11-20 10:59:47 -0600

    phase 1 part 5 completed

--- Program.cs
+++ Program.cs
@@ -25,13 +25,16 @@ namespace PlanYourHeist
             Console.Write("Please enter an integer representing your team member's skill level . . . ");
             int skillLevelResponse = int.Parse(Console.ReadLine());
             
+            Console.Write("Please enter a number, including decimal points if appropriate, representing your team member's courage factor . . . ");
+            double courageFactorResponse = double.Parse(Console.ReadLine());
+
             List<Accomplice> accomplices = new List<Accomplice>() {
                 new Accomplice () {
                     name = nameResponse, 
                     skillLevel = skillLevelResponse, 
-                    courageFactor = 0.0}
+                    courageFactor = courageFactorResponse}
             };
-            
+
         }
     }
 }
